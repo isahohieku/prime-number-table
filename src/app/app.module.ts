27ef02deb547b1a8e9ed35f5a2a3d7acc7629b5f
Isah Ohieku <isahohieku@gmail.com>
2020-10-10 13:05:31 +0100

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxBootstrapIconsModule.pick(allIcons)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
